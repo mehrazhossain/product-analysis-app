9f5dbad711299055bfc87edef0d499a2f6d46301
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-04-04 01:44:33 +0600

    basic component setup

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+  return (
+    <div>
+      <h1>This is BLOGS component</h1>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+  return (
+    <div>
+      <h1>This is REVIEWS Component</h1>
+    </div>
+  );
+};
+
+export default Reviews;
